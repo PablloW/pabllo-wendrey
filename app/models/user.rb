0187471e33e848114e8a95c996ac2f0356596ce0
PablloW <pabllowendrey@gmail.com>
2025-05-28 04:19:31 -0300

    Filter users by company changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   belongs_to :company
 
-  scope :by_company, -> (identifier) { where(company: identifier) if identifier.present? }
+  scope :by_company, -> (identifier) { 
+    joins(:company).where(companies: { identifier: identifier }) if identifier.present?
+  }
   scope :by_username, -> (username) { where('username LIKE ?', username) if username.present? }
 end
