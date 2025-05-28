2e6bccca37443f72fe575e32b441c0b5d3e03524
PablloW <pabllowendrey@gmail.com>
2025-05-28 05:33:49 -0300

    Implement cursor pagination for tweets index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,12 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.all
+    limit = params.fetch(:limit, 10).to_i
+    cursor = params[:cursor].to_i
+
+    tweets = Tweet.order(created_at: :desc)
+    tweets = tweets.where('strftime("%s", created_at) < ?', cursor) if cursor.positive?
+    tweets = tweets.limit(limit)
+
+    render json: { tweets: tweets }
   end
 end
